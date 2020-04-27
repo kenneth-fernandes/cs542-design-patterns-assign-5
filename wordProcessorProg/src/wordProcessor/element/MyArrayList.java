cbc94ff19577284bee2b82e9f42365ddcd17f562
kenneth-fernandes <kferna11@binghamton.edu>
2020-04-27 02:00:05 -0400

    Kenneth: Moved the MyArryList file to another package.

--- wordProcessorProg/src/wordProcessor/element/MyArrayList.java
+++ wordProcessorProg/src/wordProcessor/element/MyArrayList.java
@@ -1,25 +1,39 @@
-package wordProcessor.arraylist;
+package wordProcessor.element;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 
 import wordProcessor.element.ElementI;
-import wordProcessor.element.MyElement;
 import wordProcessor.util.fileprocess.FileProcessorI;
+import wordProcessor.util.validator.ValidatorFetcher;
+import wordProcessor.util.validator.ValidatorFetcherI;
+import wordProcessor.util.validator.ValidatorUtil;
+import wordProcessor.util.validator.ValidatorUtilI;
+import wordProcessor.util.validator.exceptions.InvalidInputFileFormatException;
 import wordProcessor.visitor.VisitorI;
 
 public class MyArrayList implements ElementI {
     private static MyArrayList myArrayListObj = new MyArrayList();
     private static ArrayList<ElementI> myArrayList = new ArrayList<ElementI>();
-   
 
     public static class Builder {
         private String line = "";
         private String temp = "";
 
-        public Builder withFileProcessor(FileProcessorI fileProcessor) throws IOException {
+        public Builder withFileProcessor(FileProcessorI fileProcessor)
+                throws IOException, InvalidInputFileFormatException {
+
+            // Stores the interface of ValidatorUtilI for ValidatorUtil instance
+            ValidatorUtilI validatrUtilObj = ValidatorUtil.getInstance();
+            // Stores the interface of ValidatorFetcherI for ValidatorFetcher instance
+            ValidatorFetcherI validatrFetchrObj = ValidatorFetcher.getInstance();
+
             while ((line = fileProcessor.readLine()) != null) {
+                
+                validatrUtilObj.validateInputFileData("Input-file data error",
+                        validatrFetchrObj.inputFileFormatValidn(line));
+
                 if (line.indexOf(".") >= 0) {
                     String[] lineArr = line.trim().split("\\.");
                     temp = temp.concat(lineArr[0]);
